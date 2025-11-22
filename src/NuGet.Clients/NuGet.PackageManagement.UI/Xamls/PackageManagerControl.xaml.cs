1768b3a1a50f8ad1504778fd2e3bc385667f7d6e
Jean-Pierre Briedé <jebriede@microsoft.com>
2025-11-21 16:20:23 -0800

    WIP: Added: Button in Solution Explorer Gold Bar MEF Service for handling interaction Fetching MCP Server Tools as functions Preparing Request and sending to service

--- src/NuGet.Clients/NuGet.PackageManagement.UI/Xamls/PackageManagerControl.xaml.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Xamls/PackageManagerControl.xaml.cs
@@ -73,6 +73,7 @@ namespace NuGet.PackageManagement.UI
         private IPackageVulnerabilityService _packageVulnerabilityService;
         private INuGetPackageFileService _nugetPackageFileService;
         private bool _isReadmeTabEnabled;
+        private FixVulnerabilitiesViewModel _fixVulnerabilitiesViewModel;
 
         private SearchControl SearchControl
         {
@@ -189,6 +190,8 @@ namespace NuGet.PackageManagement.UI
             // UI is initialized. Start the first search
             _packageList.CheckBoxesEnabled = _topPanel.Filter == ItemFilter.UpdatesAvailable;
             _packageList.IsSolution = Model.IsSolution;
+            _fixVulnerabilitiesViewModel = new FixVulnerabilitiesViewModel(_serviceBroker);
+            _packageList.FixVulnerabilitiesViewModel = _fixVulnerabilitiesViewModel;
 
             Loaded += PackageManagerLoaded;
 
@@ -1631,6 +1634,8 @@ namespace NuGet.PackageManagement.UI
             _packageDetail.Cleanup();
             _detailModel.Dispose();
             _packageList.SelectionChanged -= PackageList_SelectionChanged;
+
+            _fixVulnerabilitiesViewModel.Dispose();
 
             EmitPMUIClosingTelemetry();
         }
